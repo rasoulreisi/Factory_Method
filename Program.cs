cb0891b73f7965626826fc3fc874df7578c3c55e
RasoulR <rasoulr@systemgroup.net>
2023-10-31 11:51:42 +0330

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Factory_Method
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
